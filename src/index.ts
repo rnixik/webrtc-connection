eb7054a37ae865438e1602e725712072dd9c2e8f
Roman Nix <rnix@yandex.ru>
2019-12-21 00:31:33 +0700

    Add index.ts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { ManualSignaling } from './ManualSignaling';
+export { SignalingInterface } from './SignalingInterface';
+export { SocketIoSignaling } from './SocketIoSignaling';
+export { WebRtcConnection } from './WebRtcConnection';
+export { WebRtcConnectionsPool } from './WebRtcConnectionsPool';
