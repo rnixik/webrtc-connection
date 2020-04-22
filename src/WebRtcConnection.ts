bba503688dbd0806195c5fb5db0ae1404cedb1dd
Roman Nix <rnix@yandex.ru>
2020-04-22 23:53:51 +0700

    Implement framing large messages

--- src/WebRtcConnection.ts
+++ src/WebRtcConnection.ts
@@ -1,5 +1,19 @@
 import { SignalingInterface, SignalingDataInterface } from './SignalingInterface';
 
+class FramedMessage {
+  public id: string;
+  public framesNumber: number;
+  public currentFrame: number;
+  public data: string;
+
+  constructor (id: string, framesNumber: number, currentFrame: number, data: string) {
+    this.id = id;
+    this.framesNumber = framesNumber;
+    this.currentFrame = currentFrame;
+    this.data = data;
+  }
+}
+
 export class WebRtcConnection {
   private peers: {
     [key: string]: {
@@ -15,8 +29,13 @@ export class WebRtcConnection {
   private onMessageCallbacks: ((message: string, peerId: string) => void)[] = [];
   private onCloseCallbacks: (() => void)[] = [];
   private readonly id: string;
+  private readonly useFraming: boolean = false;
+  private framedPool: Map<string, FramedMessage[]> = new Map();
+  private framedTimeouts: Map<string, number> = new Map();
+  private framesTimeoutValue = 5000;
+  private maxMessageSizeForFrame: number = 15 * 1024;
 
-  constructor(signaling: SignalingInterface, iceServers: object[] = []) {
+  constructor(signaling: SignalingInterface, iceServers: object[] = [], useFraming: boolean = false) {
     this.signaling = signaling;
     this.signaling.bindInitiateOfferCallback(offerId => {
       this.initiateOffer(offerId);
@@ -24,6 +43,8 @@ export class WebRtcConnection {
     this.signaling.bindOnIncomingDataCallback((data: SignalingDataInterface) => {
       this.applyRemote(data);
     });
+
+    this.useFraming = useFraming;
 
     this.id = signaling.getId();
 
@@ -54,12 +75,14 @@ export class WebRtcConnection {
   }
 
   public sendMessage(message: string): void {
-    for (const peer in this.peers) {
-      const channel = this.peers[peer].channel;
-      if (channel !== undefined) {
-        channel.send(message);
+    this.splitMessageToFrames(message).then((preparedMessage) => {
+      for (const peer in this.peers) {
+        const channel = this.peers[peer].channel;
+        if (channel !== undefined) {
+          channel.send(preparedMessage);
+        }
       }
-    }
+    });
   }
 
   public close(): void {
@@ -160,9 +183,15 @@ export class WebRtcConnection {
       // Nothing to do
     };
     channel.onmessage = (e): void => {
-      for (const callback of this.onMessageCallbacks) {
-        callback(e.data, id);
-      }
+      this.prepareIncomingMessage(e.data).then((message) => {
+        if (typeof message === 'undefined') {
+          return;
+        }
+
+        for (const callback of this.onMessageCallbacks) {
+          callback(message, id);
+        }
+      })
     };
   }
 
@@ -209,5 +238,64 @@ export class WebRtcConnection {
         this.bindChannelEvents(id, e.channel);
       };
     }
+  }
+
+  private async splitMessageToFrames(message: string): Promise<string[]> {
+    if (!this.useFraming) {
+      return [message];
+    }
+    const framedMessages: string[] = [];
+    const id = '' + Math.floor(Math.random() * 100000000)
+    const messageBlob = new Blob([message]);
+    const messageSize = messageBlob.size;
+    const framesNumber = Math.ceil(messageSize / this.maxMessageSizeForFrame);
+    const frameSize = Math.floor(messageSize / framesNumber)
+    for (let currentFrame = 1; currentFrame <= framesNumber; currentFrame += 1) {
+      const frameData = messageBlob.slice((currentFrame - 1) * frameSize, frameSize)
+      const framedMessage = new FramedMessage(
+          id,
+          framesNumber,
+          currentFrame,
+          await frameData.text()
+      );
+      framedMessages.push(JSON.stringify(framedMessage));
+    }
+
+    return framedMessages;
+  }
+
+  private async prepareIncomingMessage(message: string): Promise<string | undefined> {
+    if (!this.useFraming) {
+      return message;
+    }
+
+    const framedMessage = (JSON.parse(message) as FramedMessage);
+
+    this.framedTimeouts.set(framedMessage.id, setTimeout(() => {
+      this.framedPool.delete(framedMessage.id)
+    }, this.framesTimeoutValue));
+
+    if (!this.framedPool.has(framedMessage.id)) {
+      this.framedPool.set(framedMessage.id, []);
+    }
+
+    const frames = this.framedPool.get(framedMessage.id);
+    if (frames) {
+      frames.push(framedMessage);
+      if (framedMessage.framesNumber === frames.length) {
+        const blobParts = [];
+        for (const framedMessage of frames) {
+          blobParts.push(framedMessage.data);
+        }
+
+        const blob = new Blob(blobParts);
+
+        return await blob.text()
+      }
+    }
+
+    // Awaiting other parts
+
+    return undefined;
   }
 }
